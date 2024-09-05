514fb2aa159f11d167fe6a9e4405211570ad7c2f
Jordan99990 <104220884+Jordan99990@users.noreply.github.com>
2024-09-05 17:01:41 +0300

    hyperparams

--- tasks/global.r
+++ tasks/global.r
@@ -1,11 +1,12 @@
 library(shiny)
 library(ggplot2)
 library(GA)
+library(plotly)
 
-# Hiperparameters
-default_func <- "x"
+# Hyperparameters
+default_func <- "x^2"
 default_start <- -10
 default_end <- 10
-default_popSize <- 50
-default_maxGen <- 100
-default_mutationRate <- 0.1
+default_popSize <- 150
+default_maxGen <- 1000
+default_mutationRate <- 0.69
